c79d2b0635dfc48dd1718ac3fe6fde7eb76c7d11
nk-on <johndoe@example.com>
2024-06-04 22:10:43 +0400

    Refactor:putting for each inside get characters instead of renderCharacters

--- src/script.js
+++ src/script.js
@@ -4,18 +4,16 @@ const characterCardTemplate = document.querySelector('#character');
 async function getCharacters() {
   const res = await fetch('	https://hp-api.onrender.com/api/characters');
   const data = await res.json();
-  renderCharacters(data);
+  data.forEach((data)=> renderCharacters(data))
 }
 function renderCharacters(data) {
-  data.forEach((data) => {
-    const characterCard = characterCardTemplate.content.cloneNode(true);
-    const characterImage = characterCard.querySelector('[data-image]');
-    const characterTitle = characterCard.querySelector('[data-name]');
-    const characterHouse = characterCard.querySelector('[data-house]');
-    characterImage.setAttribute('src', data.image);
-    characterTitle.textContent = data.name;
-    characterHouse.textContent = data.house;
-    container.appendChild(characterCard);
-  });
+  const characterCard = characterCardTemplate.content.cloneNode(true);
+  const characterImage = characterCard.querySelector('[data-image]');
+  const characterTitle = characterCard.querySelector('[data-name]');
+  const characterHouse = characterCard.querySelector('[data-house]');
+  characterImage.setAttribute('src', data.image);
+  characterTitle.textContent = data.name;
+  characterHouse.textContent = data.house;
+  container.appendChild(characterCard);
 }
 getCharacters();
