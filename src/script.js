d472cb7dcf7c4c4a309164db2e2110e9b23ad600
nk-on <johndoe@example.com>
2024-06-05 21:56:35 +0400

    Fix:fixed bug in particular app showed user only one character when initially rendering

--- src/script.js
+++ src/script.js
@@ -1,19 +1,34 @@
-const searchBar = document.querySelector('#search-characters');
 const container = document.querySelector('[data-container]');
 const characterCardTemplate = document.querySelector('#character');
+const searchBar = document.querySelector('#search-bar');
 async function getCharacters() {
   const res = await fetch('	https://hp-api.onrender.com/api/characters');
   const data = await res.json();
-  data.forEach((data)=> renderCharacters(data))
+  renderCharacters(data);
+}
+async function searchCharacters() {
+  const res = await fetch('	https://hp-api.onrender.com/api/characters');
+  const data = await res.json();
+  const searchValue = searchBar.value.trim().toLowerCase();
+  const filteredData = data.filter((data) =>
+    data.name.toLowerCase().includes(searchValue)
+  );
+  renderCharacters(filteredData);
 }
 function renderCharacters(data) {
-  const characterCard = characterCardTemplate.content.cloneNode(true);
-  const characterImage = characterCard.querySelector('[data-image]');
-  const characterTitle = characterCard.querySelector('[data-name]');
-  const characterHouse = characterCard.querySelector('[data-house]');
-  characterImage.setAttribute('src', data.image);
-  characterTitle.textContent = data.name;
-  characterHouse.textContent = data.house;
-  container.appendChild(characterCard);
+  if (container.innerHTML.length !== 0) {
+    container.innerHTML = '';
+  }
+  data.forEach((dataElement) => {
+    const characterCard = characterCardTemplate.content.cloneNode(true);
+    const characterImage = characterCard.querySelector('[data-image]');
+    const characterTitle = characterCard.querySelector('[data-name]');
+    const characterHouse = characterCard.querySelector('[data-house]');
+    characterImage.setAttribute('src', dataElement.image);
+    characterTitle.textContent = dataElement.name;
+    characterHouse.textContent = dataElement.house;
+    container.appendChild(characterCard);
+  });
 }
 getCharacters();
+searchBar.addEventListener('input', searchCharacters);
